f48dbb4b0654cd961a3fb1729be1a4084837a74c
Andrii Hereha <dikons69@gmail.com>
2022-09-05 21:27:36 +0200

    First commit

---
+++ Block/ShippingMethods.php
@@ -0,0 +1,23 @@
+<?php
+namespace Test\Shipping\Block;
+
+class ShippingMethods implements \Magento\Framework\View\Element\Block\ArgumentInterface
+{
+    private $shippingMethods;
+
+    /**
+     * ShippingMethods constructor.
+     * @param \Magento\Shipping\Model\Config\Source\Allmethods $shippingMethods
+     */
+    public function __construct(
+        \Magento\Shipping\Model\Config\Source\Allmethods $shippingMethods
+    )
+    {
+        $this->shippingMethods = $shippingMethods;
+    }
+
+    public function getShippingMethods()
+    {
+        return $this->shippingMethods->toOptionArray(true);
+    }
+}
